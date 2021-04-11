35d32cd6e8718ff36ac50d575a42a641f6440327
Lorenzo Stella <lorenzostella@gmail.com>
2021-04-12 00:25:50 +0200

    add iterations, improve allparams and dot

--- src/model.jl
+++ src/model.jl
@@ -5,21 +5,22 @@ abstract type Model end
 
 allfields(m::Model) = [getfield(m, k) for k in fieldnames(typeof(m))]
 
+_push_allparams!(c::Nothing, a::AbstractArray{T}) where T <: Number = AbstractArray{T}[a]
+_push_allparams!(c::Vector, a::AbstractArray{T}) where T <: Number = push!(c, a)
 _push_allparams!(c, ::Function) = c
-_push_allparams!(c, a::AbstractArray) = begin push!(c, a); c end
 _push_allparams!(c, t::Tuple) = begin
     for el in t
-        _push_allparams!(c, el)
+        c = _push_allparams!(c, el)
     end
     return c
 end
 _push_allparams!(c, m::Model) = begin
     for f in allfields(m)
-        _push_allparams!(c, f)
+        c = _push_allparams!(c, f)
     end
     return c
 end
-allparams(m::Model) = _push_allparams!([], m)
+allparams(m::Model) = _push_allparams!(nothing, m)
 
 overlap(m1::Model, m2::Model) = [
     p1 for (p1, p2) in zip(ProtoGrad.allparams(m1), ProtoGrad.allparams(m2))
@@ -49,13 +50,17 @@ _zero(t::Tuple) = _zero.(t)
 _zero(m::T) where T <: Model = T((_zero(f) for f in allfields(m))...)
 Base.zero(m::Model) = _zero(m)
 
-_dot(::F, ::F) where F <: Function = 0
-_dot(a1::AbstractArray, a2::AbstractArray) = dot(a1, a2)
-_dot(m1::T, m2::T) where T <: Model = begin
-    fields_m1 = allfields(m1)
-    return length(fields_m1) == 0 ? 0 : sum((_dot(a1, a2) for (a1, a2) in zip(fields_m1, allfields(m2))))
+Base.length(m::Model) = sum(length(p) for p in allparams(m))
+
+function Base.iterate(m::Model, itr=Iterators.flatten(allparams(m)))
+    y = iterate(itr)
+    if y === nothing
+        return nothing
+    end
+    val, s = y
+    return val, Iterators.rest(itr, s)
 end
-_dot(t1::Tuple, t2::Tuple) = sum(_dot.(t1, t2))
-LinearAlgebra.dot(m1::T, m2::T) where T <: Model = _dot(m1, m2)
 
 Base.show(io::IO, m::T) where T <: Model = print(io, T)
+
+LinearAlgebra.dot(m1::T, m2::T) where T <: Model = sum((dot(p1, p2) for (p1, p2) in zip(allparams(m1), allparams(m2))))
