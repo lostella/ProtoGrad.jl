4b4a01fe6b675ab3ade45dd46bec78fec3123bfc
Lorenzo Stella <lorenzostella@gmail.com>
2023-08-20 21:58:52 +0200

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,14 @@ using Test
 using Aqua
 using JET
 
-@testset "Aqua" Aqua.test_all(ProtoGrad; ambiguities=false)
+@testset "Aqua" begin
+    Aqua.test_all(ProtoGrad; ambiguities=false)
+end
 
 if VERSION >= v"1.9.0"
-    @testset "JET" JET.test_package(ProtoGrad)
+    @testset "JET" begin
+        JET.test_package(ProtoGrad)
+    end
 end
 
 include("test_utils.jl")
